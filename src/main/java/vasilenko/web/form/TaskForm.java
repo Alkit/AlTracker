02e5da3f669921fe2d2b4ce24f99c6c41ea6be42
Alkit <alkitav@gmail.com>
2017-01-29 17:29:19 +0200

    Load

---
+++ src/main/java/vasilenko/web/form/TaskForm.java
@@ -0,0 +1,86 @@
+package vasilenko.web.form;
+
+import java.sql.Date;
+
+/**
+ * Created by Alkit on 1/20/2017.
+ */
+public class TaskForm {
+    private String taskName;
+    private Integer estimateHours;
+    private Date beginDate;
+    private Date endDate;
+    private Integer executor;
+    private Integer qualification;
+    private Integer sprint;
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public Integer getEstimateHours() {
+        return estimateHours;
+    }
+
+    public void setEstimateHours(Integer estimateHours) {
+        this.estimateHours = estimateHours;
+    }
+
+    public Date getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(Date beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Integer getExecutor() {
+        return executor;
+    }
+
+    public void setExecutor(Integer executor) {
+        this.executor = executor;
+    }
+
+    public Integer getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(Integer qualification) {
+        this.qualification = qualification;
+    }
+
+    public Integer getSprint() {
+        return sprint;
+    }
+
+    public void setSprint(Integer sprint) {
+        this.sprint = sprint;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskForm{" +
+                "taskName='" + taskName + '\'' +
+                ", estimateHours=" + estimateHours +
+                ", beginDate=" + beginDate +
+                ", endDate=" + endDate +
+                ", executor=" + executor +
+                ", qualification=" + qualification +
+                ", sprint=" + sprint +
+                '}';
+    }
+
+}
--- src/main/webapp/resources/js/employeeRequest.js
+++ src/main/webapp/resources/js/employeeRequest.js
@@ -4,12 +4,31 @@ $('.request').on('click',function () {
     $('#taskIdForRequest').val(value);
 })
 
+$('.complete').on('click',function () {
+    var value = $(this).attr('value');
+    $('#completeForm').modal('show');
+    $('#taskForComplete').val(value);
+    console.log($('#taskForComplete').attr('value'))
+})
 
 $('#taskRequest').form({
     on: ['blur','submit'],
     fields: {
         time:{
             identifier: 'time',
+            rules: [{
+                type: 'integer',
+                prompt : 'Please enter a value'
+            }]
+        }
+    }
+})
+
+$('#completeF').form({
+    on: ['blur','submit'],
+    fields: {
+        time:{
+            identifier: 'timeSpent',
             rules: [{
                 type: 'integer',
                 prompt : 'Please enter a value'
